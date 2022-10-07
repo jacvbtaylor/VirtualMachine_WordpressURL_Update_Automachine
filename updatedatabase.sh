76fbca1a372c10ebbe4cb3bb89ffeebcb99bfe27
Jacob Taylor <100665980+jacvbtaylor@users.noreply.github.com>
2022-10-06 23:24:54 -0700

    Create updatedatabase.sh

---
+++ updatedatabase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#change enp0s3 to your appropiate network
+
+sql_statement()
+{
+        ip4=$(/sbin/ip -o -4 addr list enp0s3 | awk '{print$4}' | cut -d/ -f1)
+        echo "use wordpress"
+        echo "UPDATE wp_options SET option_value=\"http://${ip4}/wordpress\" WHERE option_name = \"home\";"
+        echo "UPDATE wp_options SET option_value=\"http://${ip4}/wordpress\" WHERE option_name = \"siteurl\";"
+}
+
+mysql -u root -ppassword wordpress < <(sql_statement)
